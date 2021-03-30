3bf031b2360297ac4d56c0ece481075b31e2705e
Logan Smithson <70979203+Ldsmiths@users.noreply.github.com>
2021-03-30 14:00:44 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb+srv://ldsmiths:L2Sydh450ETCndK3@cluster0.we2jc.mongodb.net/notesDB", { useNewUrlParser: true }, { useUnifiedTopology: true })
+
+//create a data schema
+const notesSchema = {
+    title: String,
+    content: String
+}
+
+const Note = mongoose.model("Note", notesSchema);
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/", function (req, res) {
+    let newNote = new Note({
+        title: req.body.title,
+        content: req.body.content
+    });
+    newNote.save();
+    res.redirect('/');
+})
+
+app.listen(3000, function () {
+    console.log("server is running on andre 3000");
+})
